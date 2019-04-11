3dd9668e0f8433773aac95e0d6a7b8b85dfedbc3
SiAnton <ant.sinash@gmail.com>
2019-04-12 00:35:42 +0700

    New version 8

--- ProfSkill/settings.py
+++ ProfSkill/settings.py
@@ -146,7 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 import dj_database_url
 db_from_env = dj_database_url.config()
